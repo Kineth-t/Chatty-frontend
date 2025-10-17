2495d98ecae9613dacd2c8296280d2468e0e6169
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-17 17:10:05 +0800

    Created LoadingScreen component.

---
+++ src/components/auth/LoadingScreen.jsx
@@ -0,0 +1,8 @@
+export default LoadingScreen() (
+  <div className="login-container">
+    <div className="login-box">
+      <h1 className="login-title">Chatty</h1>
+      <p style={{ textAlign: 'center', color: '#666' }}>Loading...</p>
+    </div>
+  </div>
+);
