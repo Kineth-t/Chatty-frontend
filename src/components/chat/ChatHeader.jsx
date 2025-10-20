4647f8ce3b8c2f880d3daac31ff1edd81c2f21cc
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-20 13:27:08 +0800

    Implemented ChatHeader.jsx.

---
+++ src/components/chat/ChatHeader.jsx
@@ -0,0 +1,12 @@
+export default function ChatHeader({ selectedUser })
+{
+  <div className="chat-header">
+    <div className="chat-user-avatar">
+      {selectedUser.username[0].toUpperCase()}
+    </div>
+    <div>
+      <h3 className="chat-username">{selectedUser.username}</h3>
+      <p className="chat-status">Online</p>
+    </div>
+  </div>
+};
