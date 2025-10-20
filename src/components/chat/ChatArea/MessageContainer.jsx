10afa65151d13c2d95bc4c207f42d5e1a744c2dc
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-20 13:30:52 +0800

    Implemented MessageContainer.jsx.

---
+++ src/components/chat/ChatArea/MessageContainer.jsx
@@ -0,0 +1,15 @@
+import { Message } from './Message';
+
+export default function MessagesContainer({ messages, currentUserUsername }) {
+    return (
+        <div className="messages-container">
+            {messages.map((msg, index) => (
+            <Message
+                key={msg.id || index}
+                msg={msg}
+                currentUserUsername={currentUserUsername}
+            />
+            ))}
+        </div>
+    )
+}
